b18439734ecceef0f189fcee7e93f32a4d22155c
CyrilBaah <cyril.baah@amalitech.org>
2023-05-20 21:21:37 +0000

    update a book

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import BookCreateAPIView, BookListAPIView
+from .views import BookCreateAPIView, BookListAPIView, BookUpdateAPIView
 
 urlpatterns = [
     path("create/", BookCreateAPIView.as_view(), name="create_book"),
     path("", BookListAPIView.as_view(), name="list_book"),
+    path("update/<int:pk>/", BookUpdateAPIView.as_view(), name="update_book"),
 ]
--- books/views.py
+++ books/views.py
@@ -20,18 +20,46 @@ class BookCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+
 class BookListAPIView(APIView):
     """List all Book"""
-    
-    permission_classes = (IsAuthenticated, )
+
+    permission_classes = (IsAuthenticated,)
     authentication_classes = (JWTAuthentication,)
-    
+
     def get(self, request):
         serializer = BookSerializer()
-        
+
         books = Book.objects.all()
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-        
-    
+
+
+class BookUpdateAPIView(APIView):
+    """Update a Book | Administrator"""
+
+    permission_classes = (IsAuthenticated, IsAdminUser)
+    authentication_classes = (JWTAuthentication,)
+
+    serializer_class = BookSerializer
+
+    def put(self, request, pk):
+        book = Book.objects.filter(id=pk).first()
+        if not book:
+            return Response(
+                {"detail": "Book not found"}, status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = BookSerializer(book, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            data = serializer.data
+            response = {
+                "status": "success",
+                "code": status.HTTP_200_OK,
+                "message": "Book Updated successfully",
+                "data": data,
+            }
+            return Response(response, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
