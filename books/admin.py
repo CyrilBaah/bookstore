aa803d084790adb24a86be883cd00905df4fc5c8
CyrilBaah <cyril.baah@amalitech.org>
2023-06-01 21:13:12 +0000

    admin view

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Book
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "price", "isbn"]
+    list_filter = ["title", "author", "price", "isbn"]
+    search_fields = ["title", "author", "price", "isbn"]
+    
