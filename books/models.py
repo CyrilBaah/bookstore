ba3a61bec03410ddb794980a770b8cef0d01bf26
CyrilBaah <cyril.baah@amalitech.org>
2023-06-17 16:49:46 +0000

    list and update book

--- books/models.py
+++ books/models.py
@@ -8,4 +8,4 @@ class Book(models.Model):
     author = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     isbn = ISBNField()
-    cover_image = models.ImageField(upload_to="book_covers/")
+    cover_image = models.ImageField(upload_to="book_covers/", null=True, blank=True)
