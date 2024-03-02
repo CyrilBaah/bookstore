20916e9ddb92b347dde4d8aeb0f3246684dcd485
CyrilBaah <baah_yard12@yahoo.com>
2024-03-02 18:49:37 +0000

    fixes

--- accounts/tests/tests_logout.py
+++ accounts/tests/tests_logout.py
@@ -1,10 +1,10 @@
-from django.contrib.auth import get_user_model
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-from rest_framework_simplejwt.tokens import RefreshToken
+# from django.contrib.auth import get_user_model
+# from django.urls import reverse
+# from rest_framework import status
+# from rest_framework.test import APITestCase
+# from rest_framework_simplejwt.tokens import RefreshToken
 
-User = get_user_model()
+# User = get_user_model()
 
 
 # class UserLogoutAPIViewTestCase(APITestCase):
